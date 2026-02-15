a53c3e9e729b5012dbce0e4a31389c6194f9ee48
Sadat2004-hub <hotelesdeoaxaca@gmail.com>
2026-02-15 12:42:45 -0600

    style: optimize admin layout and viewport settings

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -3,6 +3,15 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
+export const viewport = {
+    themeColor: '#0047AB',
+}
+
+
 export default function AdminPage() {
-    return <NextStudio config={config} />
+    return (
+        <div style={{ margin: 0, padding: 0, height: '100vh' }}>
+            <NextStudio config={config} />
+        </div>
+    )
 }
