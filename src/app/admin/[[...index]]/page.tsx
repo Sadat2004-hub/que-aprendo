af1b999eef5dbe36521aeb5b9532a677debc1fea
Sadat2004-hub <hotelesdeoaxaca@gmail.com>
2026-02-15 12:56:44 -0600

    fix: update next-sanity and enable proper admin exports

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -5,6 +5,9 @@ import config from '../../../../sanity.config'
 
 export const dynamic = 'force-static'
 
+export { metadata } from 'next-sanity/studio/metadata'
+export { viewport } from 'next-sanity/studio/viewport'
+
 export default function AdminPage() {
     return <NextStudio config={config} />
 }
