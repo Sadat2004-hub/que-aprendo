e85d90a118da4f3431d1022bff4f691df7077ec4
Sadat2004-hub <hotelesdeoaxaca@gmail.com>
2026-02-18 12:54:08 -0600

    Fix: match admin configuration with MascotasOaxaca and OaxacaFit

--- src/app/admin/[[...index]]/page.tsx
+++ src/app/admin/[[...index]]/page.tsx
@@ -1,14 +1,7 @@
 'use client'
 
-import nextDynamic from 'next/dynamic'
+import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
-
-const NextStudio = nextDynamic(
-    () => import('next-sanity/studio').then((mod) => mod.NextStudio),
-    { ssr: false }
-)
-
-export const dynamic = 'force-dynamic'
 
 export default function AdminPage() {
     return <NextStudio config={config} />
--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,7 +1,7 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-30'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+export const dataset = 'production'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+export const projectId = '20swkfwv'
 
-export const useCdn = true
+export const useCdn = false
